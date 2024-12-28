71e283fb9ce1c47f1234235fbedb6e7a734aa609
congnghiahieu <21020540@vnu.edu.vn>
2024-12-28 11:51:56 +0700

    [pysrc2cpg] fix null value

--- console/src/main/scala/io/joern/console/cpgcreation/PythonSrcCpgGenerator.scala
+++ console/src/main/scala/io/joern/console/cpgcreation/PythonSrcCpgGenerator.scala
@@ -11,13 +11,12 @@ import scala.compiletime.uninitialized
 import scala.util.Try
 
 case class PythonSrcCpgGenerator(config: FrontendConfig, rootPath: Path) extends CpgGenerator {
-  private lazy val command: Path = if (isWin) rootPath.resolve("pysrc2cpg.bat") else rootPath.resolve("pysrc2cpg")
-  private var typeRecoveryConfig: XTypeRecoveryConfig = uninitialized
+  private lazy val command: Path      = if (isWin) rootPath.resolve("pysrc2cpg.bat") else rootPath.resolve("pysrc2cpg")
+  private lazy val typeRecoveryConfig = XTypeRecoveryConfig.parse(config.cmdLineParams.toSeq)
 
   /** Generate a CPG for the given input path. Returns the output path, or None, if no CPG was generated.
     */
   override def generate(inputPath: String, outputPath: String = "cpg.bin.zip"): Try[String] = {
-    typeRecoveryConfig = XTypeRecoveryConfig.parse(config.cmdLineParams.toSeq)
     val arguments = Seq(inputPath, "-o", outputPath) ++ config.cmdLineParams
     runShellCommand(command.toString, arguments).map(_ => outputPath)
   }
